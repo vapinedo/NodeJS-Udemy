c07e12123e0caf0cd6dacf04959d1e6e209efe6f
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-04-08 07:42:49 -0500

    [ADD] recibir parametros de la consola

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 
-const { crearArchivo } = require("./helpers/multiplicar");
+const { crearArchivo } = require('./helpers/multiplicar');
 
 console.clear();
 
-const [,, arg3 = "base=4"] = process.argv;
-const [, base] = arg3.split("=");
+const [,, arg3 = 'base=4'] = process.argv;
+const [, base = 5] = arg3.split('=');
 
 crearArchivo(base)
     .then(console.log)
